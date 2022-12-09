60d67b8518b646fdf46374875bd6511e7b36e1a6
GillianCharra <gillian@charra.fr>
2022-12-09 12:13:14 +0100

    refacto

--- Documents/EPSI/2eannee/C#/GILLIAN_nombreromain/GILLIAN_nombreromain.test/UnitTest1.cs
+++ Documents/EPSI/2eannee/C#/GILLIAN_nombreromain/GILLIAN_nombreromain.test/UnitTest1.cs
@@ -2,39 +2,12 @@ namespace GILLIAN_nombreromain.test
 {
     public class NombresRomainsTest
     {
-        [Theory]
-        [InlineData(0)]
-        [InlineData(1)]
-        [InlineData(2)]
-        [InlineData(3)]
-        [InlineData(10)]
-        [InlineData(11)]
-        [InlineData(12)]
-        [InlineData(13)]
-        [InlineData(20)]
-        [InlineData(21)]
-        [InlineData(22)]
-        [InlineData(23)]
-        [InlineData(30)]
-        [InlineData(4)]
-        [InlineData(14)]
-        [InlineData(24)]
-        [InlineData(5)]
-        [InlineData(6)]
-        [InlineData(7)]
-        [InlineData(8)]
-        [InlineData(15)]
-        [InlineData(16)]
-        [InlineData(17)]
-        [InlineData(18)]
-        [InlineData(25)]
-        [InlineData(26)]
-        [InlineData(27)]
-        [InlineData(28)]
-        [InlineData(9)]
-        [InlineData(19)]
-        [InlineData(29)]
-        public void TestUnites0a3puis10a13et20a23(int n)
+        [Theory, InlineData(0), InlineData(1), InlineData(2), InlineData(3), InlineData(10), InlineData(11),
+         InlineData(12), InlineData(13), InlineData(20), InlineData(21), InlineData(22), InlineData(23), InlineData(30),
+         InlineData(4), InlineData(14), InlineData(24), InlineData(5), InlineData(6), InlineData(7), InlineData(8),
+         InlineData(15), InlineData(16), InlineData(17), InlineData(18), InlineData(25), InlineData(26), InlineData(27),
+         InlineData(28), InlineData(9), InlineData(19), InlineData(29)]
+        public void Tests(int n)
         {
             int DizaineArabe = 0;
             if (n.ToString().Length >= 2)
@@ -59,7 +32,6 @@ namespace GILLIAN_nombreromain.test
                     attendu += "IX";
                     break;
             }
-
             Assert.Equal(attendu, resultat);
         }
     }
